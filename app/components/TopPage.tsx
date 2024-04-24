d2ef26ddad23bf5345f155a6f7354f13eaabfcd9
itscreek <uraitsu2000@gmail.com>
2024-04-24 11:35:59 +0900

    Add page styles

--- app/components/TopPage.tsx
+++ app/components/TopPage.tsx
@@ -1,6 +1,6 @@
 export default function TopPage() {
   return (
-    <div>
+    <div className="top-page">
       <h1>Welcome to Quantum Typing!</h1>
       <button>Start</button>
     </div>
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,12 +3,16 @@ import TopPage from "~/components/TopPage";
 import GamePage from "~/components/GamePage";
 import { LinksFunction } from "@remix-run/node";
 import globalStyle from "~/styles/global.css?url";
+import topPageStyle from "~/styles/topPage.css?url";
+import gamePageStyle from "~/styles/gamePage.css?url";
 import typingBoxStyle from "~/styles/typingBox.css?url";
 import downloadBoxStyle from "~/styles/downloadBox.css?url";
 import endGameBoxStyle from "~/styles/endGameBox.css?url";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: globalStyle },
+  { rel: "stylesheet", href: topPageStyle },
+  { rel: "stylesheet", href: gamePageStyle },
   { rel: "stylesheet", href: typingBoxStyle },
   { rel: "stylesheet", href: downloadBoxStyle },
   { rel: "stylesheet", href: endGameBoxStyle },
